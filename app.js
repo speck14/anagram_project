a8b69a33cd54d79931be0529819ce9a7d3b37407
Shannon Peck <smpeck14@gmail.com>
2021-08-28 21:24:55 -0700

    cleaned up and annotated

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 /*To do:
--Clean up orderStr 
-    let varName = orderStr()
--Display results better
--Console input: for invalid input line
+-Node (process.argv)
 -Sources list
 -Readme
 */
 
+// Convert strings to arrays in order to alphebatize and remove whitespace, then back to strings
+function orderStr (str) {
+    //Remove white space from entire string, source: https://stackoverflow.com/a/61140195
+    removeWhiteSpaceOne = str.toLowerCase().replace(/\s+/g, ''); 
+     //Quotes to create array by character, not by word
+    arrOne = removeWhiteSpaceOne.split('');   
+    //Alphabetizes the array, then reverts back to string
+    alphabetizedStr = arrOne.sort().toString();
+    return alphabetizedStr
+}
+
 function isAnagram (stringOne, stringTwo) {
-    orderStr(stringOne, stringTwo);
-    if ((alphabetizedStrOne.length > 0) && (alphabetizedStrTwo.length > 0)) {
+    let strToCompareOne = orderStr(stringOne);
+    let strToCompareTwo = orderStr(stringTwo);
+    //If spliced & alphabetized strings not empty
+    if ((strToCompareOne.length > 0) && (strToCompareTwo.length > 0)) {
         console.log(`Input: ${stringOne}, ${stringTwo}`)
-        if (alphabetizedStrOne === alphabetizedStrTwo) {
-            console.log('They match! What you have there is an anagram!')
+        if (strToCompareOne === strToCompareTwo) {
+            console.log('They match! What you have there is an anagram!\n')
         }
         else {
-            console.log('No anagrams here. Try again.')
+            console.log('No anagrams here. Try again.\n')
         }
     }
+    //If spliced strings return empty (meaning input was whitespace only)
     else {
-        console.log('Invalid input, no characters to compare.')
+        console.log(`Input: whitespace only`, 'Invalid input, no characters to compare.\n')
     }
 };
 
-// Convert strings to arrays in order to alphebatize, then back to strings
-function orderStr (strOne, strTwo) {
-    //Remove white space from entire string, source: https://stackoverflow.com/a/61140195
-    removeWhiteSpaceOne = strOne.toLowerCase().replace(/\s+/g, '');
-    removeWhiteSpaceTwo = strTwo.toLowerCase().replace(/\s+/g, '');
-    arrOne = removeWhiteSpaceOne.split('');    //Qquotes to create array by character, not by word
-    arrTwo = removeWhiteSpaceTwo.split('');
-    alphabetizedStrOne = arrOne.sort().toString();
-    alphabetizedStrTwo = arrTwo.sort().toString();
-    return [alphabetizedStrOne, alphabetizedStrTwo]
-}
 
 
-isAnagram('This is a perfect match', 'This is a perfect match');
-isAnagram('So is THIS', 'so is this');
-isAnagram('123456', '654321');
-isAnagram('Sun    flowers', 'Sunflowers');
-isAnagram('These do not match', 'Sure do not');
-isAnagram('Troy and Abed', 'In the morning')
-isAnagram('    ', '       ')
+console.log(process.argv)
+// isAnagram('This is a perfect match', 'This is a perfect match');
+// isAnagram('So is THIS', 'so is this');
+// isAnagram('123456', '654321');
+// isAnagram('Sun    flowers', 'Sunflowers');
+// isAnagram('These do not match', 'Sure do not');
+// isAnagram('Troy and Abed', 'In the morning')
+// isAnagram('    ', '       ')
+// isAnagram('%$%', '%%$')
